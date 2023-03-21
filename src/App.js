c153defe22e6e2df785d8ad00f4bf1f11e0c839c
fabrizio contreras <contrerasfabrizio13@gmail.com>
2023-03-21 16:11:28 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {Route, Routes } from "react-router-dom";
+import Footer from "./components/public/Footer";
+import Main from "./components/public/Main";
+import NavBar from "./components/public/NavBar";
+import Details from "./components/public/Details";
+import UserForm from "./components/public/UserForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+
+      <Routes>
+        {/* para llamar el element tiene que ser "{<element/>}" */}
+        <Route exact path={"/"} element={<Main/>} />
+        <Route path={"/details/:id"} element={Details} />
+        <Route path={"/user-form"} element={UserForm} />
+      </Routes>
+
+      <Footer />
+      
     </div>
   );
 }
